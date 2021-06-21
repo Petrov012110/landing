709950fa56a84222ce28f80c18743dd1bd9844ab
vadimIbragimov <68374160+vadimIbragimov@users.noreply.github.com>
2021-06-21 21:53:56 +0500

    initial commit

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,57 +1,44 @@
-import ReactDOM from 'react-dom';
-import { Turn as Hamburger } from 'hamburger-react'
-import './index.less';
-import { useState } from 'react';
+import ReactDOM from "react-dom";
+import { Sling, Turn as Hamburger } from "hamburger-react";
+import "./index.less";
+import { Menu } from "./Menu";
+import React, { useState } from "react";
+import { Button, Dropdown } from "antd";
 
-const logo = require('../../assets/images/12.png');
+const logo = require("../../assets/images/12.png");
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="header__inner">
+          <div className="header__logo">
+            <a href="#">
+              <img className="logo-header" src={logo?.default} alt="logo" />
+            </a>
+          </div>
+          <Menu className="qweqweq" mode="horizontal" />
+          <div className="burger-menu">
+            <Dropdown
+              className="my-menu-button"
+              overlay={() => <Menu mode="vertical" />}
+              visible={menuOpen}
+              placement="bottomRight"
+              trigger={["click"]}
+              onVisibleChange={(visible) => setMenuOpen(visible)}
+            >
+              <Button type="link">
+                <Sling toggled={menuOpen} />
+              </Button>
+            </Dropdown>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
 
-    const [isOpen, setOpen] = useState(false)
-    return (
-        <header className="header">
-            <div className="container">
-                <div className="header__inner">
-                    <div className="header__logo">
-                        <a href="#">
-                            <img className="logo-header" src={logo?.default} alt="logo" />
-                        </a>
-                    </div>
-                    <div className="headr__nav">
-                        <nav className="menu">
-                            <ul className="menu_list">
-                                <li className="active">
-                                    <a href="#">Поиск</a>
-                                </li>
-                            </ul>
-                        </nav>
-                        <div className="header__login">
-                            <a href="" className="header__btn">Войти</a>
-                            <a href="" className="header__btn-registration">Регистрация</a>
-                        </div>
-                    </div>
-                    <div className="burger-menu">
-                        <Hamburger 
-                            toggled={isOpen} 
-                            toggle={setOpen}
-                            onToggle={toggled => {
-                                if (toggled) {
-                                   
-                                } else {
-                                   // close a menu
-                                }
-                              }} />
-                    </div>
-                </div>
-            </div>
-        </header>
-    )
+ReactDOM.render(<Header />, document.getElementById("root"));
 
-}
-
-ReactDOM.render(
-    <Header />,
-    document.getElementById('root')
-);
-
-export default Header
+export default Header;
