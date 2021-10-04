793df6cc76142809a900c5ba20cfb1ef0b4db35d
vadimIbragimov <68374160+vadimIbragimov@users.noreply.github.com>
2021-10-04 22:10:01 +0500

    added burger menu animated icon

--- src/components/Header/Menu.tsx
+++ src/components/Header/Menu.tsx
@@ -1,21 +1,34 @@
-import { MenuFoldOutlined } from "@ant-design/icons";
 import React, { useState } from "react";
-import { slide as Menu } from 'react-burger-menu'
-
-
+import { slide as Menu } from "react-burger-menu";
+import { Fade as Hamburger } from "hamburger-react";
 
 export const MenuBurger = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
 
   const showSettings = (event: any) => {
     event.preventDefault();
-  }
+  };
 
   return (
-    <Menu id={ "sidebar" } customBurgerIcon={ <MenuFoldOutlined /> }>
-      <a id="home" className="menu-item" href="/">Home</a>
-      <a id="about" className="menu-item" href="/about">About</a>
-      <a id="contact" className="menu-item" href="/contact">Contact</a>
-      <a onClick={showSettings} className="menu-item--small" href="">Settings</a>
+    <Menu
+      id={"sidebar"}
+      isOpen={isMenuOpen}
+      customBurgerIcon={<Hamburger toggled={isMenuOpen} />}
+      onOpen={() => setIsMenuOpen(true)}
+      onClose={() => setIsMenuOpen(false)}
+    >
+      <a id="home" className="menu-item" href="/">
+        Home
+      </a>
+      <a id="about" className="menu-item" href="/about">
+        About
+      </a>
+      <a id="contact" className="menu-item" href="/contact">
+        Contact
+      </a>
+      <a onClick={showSettings} className="menu-item--small" href="">
+        Settings
+      </a>
     </Menu>
   );
 };
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -10,56 +10,54 @@ const logo = require("../../assets/images/lable.svg");
 const iconSearch = require("../../assets/images/searchIcon.svg");
 
 const Header = () => {
-    const [menuOpen, setMenuOpen] = useState(false);
-    return (
-        <header className="header">
-            <div className="container">
-                <div className="header__inner">
-                    <div className="aside">
-                        <div className="logo">
-                            <a href="#">
-                                <img src={logo?.default} alt="logo" />
-                                <span className="logotype">GetOffers</span>
-                            </a>
-                        </div>
-                        <div className="navigation">
-                            <ul>
-                                <li>                                   
-                                    <a href="#abilities">Преимущества</a>
-                                </li>
-                                <li>                                   
-                                    <a href="#carousel">Принцип работы</a>
-                                </li>
-                                <li>                                    
-                                    <a href="#props">Возможности</a>
-                                </li>
-                                <li>
-                                    <a href="#description">О сервисе</a>
-                                </li>
-                                <li>                                    
-                                    <a href="#footer">Контакты</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div className="bside">
-                        {/* <div className="burger">
-                            <MenuBurger />
-                        </div> */}
+  const [menuOpen, setMenuOpen] = useState(false);
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="header__inner">
+          <div className="aside">
+            <div className="logo">
+              <a href="#">
+                <img src={logo?.default} alt="logo" />
+                <span className="logotype">GetOffers</span>
+              </a>
+            </div>
+            <div className="navigation">
+              <ul>
+                <li>
+                  <a href="#abilities">Преимущества</a>
+                </li>
+                <li>
+                  <a href="#carousel">Принцип работы</a>
+                </li>
+                <li>
+                  <a href="#props">Возможности</a>
+                </li>
+                <li>
+                  <a href="#description">О сервисе</a>
+                </li>
+                <li>
+                  <a href="#footer">Контакты</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="bside">
+            <div className="burger">
+              <MenuBurger />
+            </div>
 
-                        <div className="search__btn">
-                            <button>
-                                <SearchOutlined />
-                                <span className="btn__name">
-                                    Найти
-                                </span>
-                            </button>
-                        </div>
-                        <div className="registration__btn">
-                            <button>Регистрация</button>
-                        </div>
-                    </div>
-                    {/* <div className="header__inner-bside">
+            <div className="search__btn">
+              <button>
+                <SearchOutlined />
+                <span className="btn__name">Найти</span>
+              </button>
+            </div>
+            <div className="registration__btn">
+              <button>Регистрация</button>
+            </div>
+          </div>
+          {/* <div className="header__inner-bside">
                         <div className="menu-web">
                             <Menu className="web" mode="horizontal" />
                         </div>
@@ -78,10 +76,10 @@ const Header = () => {
                             </Dropdown>
                         </div>
                     </div> */}
-                </div>
-            </div>
-        </header>
-    );
+        </div>
+      </div>
+    </header>
+  );
 };
 
 ReactDOM.render(<Header />, document.getElementById("root"));
